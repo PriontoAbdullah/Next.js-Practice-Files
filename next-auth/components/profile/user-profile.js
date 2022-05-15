e8720ff7308437aebcff94fb32faaf9438fc9201
PriontoAbdullah <priontosoft@gmail.com>
2022-05-15 13:24:28 +0600

    Added password change function

--- next-auth/components/profile/user-profile.js
+++ next-auth/components/profile/user-profile.js
@@ -23,10 +23,25 @@ function UserProfile() {
     return <p className={classes.profile}>Loading...</p>;
   }
 
+  // change password function
+  async function changePasswordHandler(passwordData) {
+    const response = await fetch('/api/user/change-password', {
+      method: 'PATCH',
+      body: JSON.stringify(passwordData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+  }
+
   return (
     <section className={classes.profile}>
       <h1>Your User Profile</h1>
-      <ProfileForm />
+      <ProfileForm onChangePassword={changePasswordHandler} />
     </section>
   );
 }
